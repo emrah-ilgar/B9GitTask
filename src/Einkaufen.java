9f6ba5664509656d5c83acae8fec8723b971ace9
Emrah <emrahilgar62@gmail.com>
2023-07-05 15:16:26 +0200

    Einkaufen branch file added

---
+++ src/Einkaufen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Einkaufen {
+    public static void main(String[] args) {
+        String []einKaufList=new String[5];
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Bitte schreiben dass,was möchten Sie kaufen");
+
+
+        for (int i=0; i < einKaufList.length; i++) {
+            einKaufList[i]=scanner.nextLine();
+            System.out.println("einKaufList[i] = " + einKaufList[i]);
+
+        }
+        for (int i = 0; i < einKaufList.length; i++) {
+            System.out.println(i+1+"."+einKaufList[i]);
+        }
+
+
+
+    }
+}
