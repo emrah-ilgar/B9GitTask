1d81b7a801d0866767ddc8660121c5e18fea7895
Emrah <emrahilgar62@gmail.com>
2023-07-05 16:50:53 +0200

    EinkaufenTest file and Einkaufen package added

--- src/Einkaufen/Einkaufen.java
+++ src/Einkaufen/Einkaufen.java
@@ -1,3 +1,5 @@
+package Einkaufen;
+
 import java.util.Scanner;
 
 public class Einkaufen {
--- src/Einkaufen/EinkaufenTest.java
+++ src/Einkaufen/EinkaufenTest.java
@@ -1,3 +1,7 @@
+package Einkaufen;
+
+import Einkaufen.Einkaufen;
+
 public class EinkaufenTest {
     public static void main(String[] args) {
         Einkaufen einkaufen=new Einkaufen();
