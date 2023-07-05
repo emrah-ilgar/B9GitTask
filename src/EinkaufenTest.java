8d33f287c30ece5c78e54b3c8a487096f6dc265b
Emrah <emrahilgar62@gmail.com>
2023-07-05 16:38:33 +0200

    EinkaufenTest file added

--- src/EinkaufenTest.java
+++ src/EinkaufenTest.java
@@ -1,7 +1,6 @@
 public class EinkaufenTest {
     public static void main(String[] args) {
-
-        Einkaufen einkaufen1=new Einkaufen();
-        einkaufen1.einkaufen();
+        Einkaufen einkaufen=new Einkaufen();
+        einkaufen.einkaufen();
     }
 }
